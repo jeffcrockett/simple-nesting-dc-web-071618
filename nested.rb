84cb75405a672ee283071352d1f2131f7a381c31
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-01 21:34:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -65,10 +65,7 @@ def adding_matz
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
-  programmer_hash[:yukihiro_matsumoto] = {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
+
 # }
 
 	programmer_hash = 
@@ -86,7 +83,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
     
 end
 
